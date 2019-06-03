01a66a09ce37af94c9700dbeaefde1766eab0b30
unclecode <unclecode@kidocode.com>
2019-06-03 21:18:30 +0800

    adding states structures

---
+++ src/automatas/todomata/index.js
@@ -0,0 +1,28 @@
+import Appomata from 'Appomata'
+
+import states from './states'
+import views from './views'
+
+let todomata = Appomata.createAutomata({
+    name: "todo",
+    context: {
+        all: []
+    },
+    buffer: {
+        keys: {},
+        url: {
+            all: `https://jsonplaceholder.typicode.com/todos`
+        }
+    } 
+})
+
+for (const state of states) {
+    todomata.addState(state)
+}
+for (const view of views) {
+    todomata.addView(view)
+}
+// move to first state
+todomata.init("init")
+
+export default todomata;
